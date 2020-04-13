afde86ca4acdc1a14f5d998825191cdb938820a0
yunho LEE <mmm01230@gmail.com>
2020-04-13 15:10:24 +0900

    Added 과제1 완료!

--- src/main/java/kr/ac/ks/app/IndexController.java
+++ src/main/java/kr/ac/ks/app/IndexController.java
@@ -1,14 +1,12 @@
-package kr.ac.ks.app.controller;
+package kr.ac.ks.app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
-
 }
